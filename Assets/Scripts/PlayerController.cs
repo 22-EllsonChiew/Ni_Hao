8344dac54050455fdaf9dc58362ff40828695dd9
Itzuzume <ellson637@gmail.com>
2022-01-21 01:02:43 +0800

    Update 4
    added player scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed;
+
+    Rigidbody playerRb;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0.0f, vertical);
+
+        transform.position += moveDirection * speed;
     }
 }
